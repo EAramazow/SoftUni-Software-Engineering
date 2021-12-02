aab00d38a973aaa88d18b33cdb27f01fa37a9ef8
Emil Aramazov <88932265+emilaramazow@users.noreply.github.com>
2021-12-02 19:31:11 +0200

    Delete Exercise directory

--- Exercise/P01AdvertisementMessage/Parts.java
+++
@@ -1,8 +0,0 @@
-package objectsAndClasses.Exercise.P01AdvertisementMessage;
-
-public class Parts {
-    private String phrases;
-    private String events;
-    private String authors;
-    private String cities;
-}
