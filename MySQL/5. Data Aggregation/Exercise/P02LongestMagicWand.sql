3375eb26acbdbc9fd976f583383c53884152b27e
Emil Aramazov <88932265+emilaramazow@users.noreply.github.com>
2022-05-14 18:13:59 +0300

    Add files via upload

---
+++ MySQL/5. Data Aggregation/Exercise/P02LongestMagicWand.sql
@@ -0,0 +1,4 @@
+SELECT 
+    MAX(`magic_wand_size`) AS `longest_magic_rand`
+FROM
+    `wizzard_deposits`;
---
+++ MySQL/5. Data Aggregation/Exercise/P13EmployeesAverageSalaries.sql
@@ -0,0 +1,13 @@
+SELECT 
+    department_id,
+    IF(department_id = 1,
+        AVG(salary) + 5000,
+        AVG(salary)) AS 'avg_salary'
+FROM
+    employees
+WHERE
+    salary > 30000
+        AND manager_id NOT LIKE 42
+GROUP BY department_id
+ORDER BY department_id
+;
