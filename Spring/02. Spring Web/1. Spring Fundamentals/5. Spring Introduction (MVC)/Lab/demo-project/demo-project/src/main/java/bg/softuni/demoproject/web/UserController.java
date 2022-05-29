abcd5a7afbc6b8fba502dae384a7e6b6d99a25cf
emilaramazow <emil.aramazow@gmail.com>
2022-05-29 21:41:21 +0300

    Added Spring Introduction MVC

---
+++ Spring/02. Spring Web/1. Spring Fundamentals/5. Spring Introduction (MVC)/Lab/demo-project/demo-project/src/main/java/bg/softuni/demoproject/web/UserController.java
@@ -0,0 +1,23 @@
+package bg.softuni.demoproject.web;
+
+import bg.softuni.demoproject.model.UserDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String newUser() {
+        return "newuser";
+    }
+
+    @PostMapping
+    public String createUser(UserDTO userDTO) {
+        System.out.println("Creating new user... " + userDTO);
+        return "usercreated";
+    }
+}
