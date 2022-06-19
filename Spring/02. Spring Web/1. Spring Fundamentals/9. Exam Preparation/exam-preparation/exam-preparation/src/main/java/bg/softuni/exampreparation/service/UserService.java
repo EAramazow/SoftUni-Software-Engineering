8d5845b755ac6ec6677ca6d154d4784f9a9bbb66
emilaramazow <emil.aramazow@gmail.com>
2022-06-19 21:39:36 +0300

    Added successful user login form

--- Spring/02. Spring Web/1. Spring Fundamentals/9. Exam Preparation/exam-preparation/exam-preparation/src/main/java/bg/softuni/exampreparation/service/UserService.java
+++ Spring/02. Spring Web/1. Spring Fundamentals/9. Exam Preparation/exam-preparation/exam-preparation/src/main/java/bg/softuni/exampreparation/service/UserService.java
@@ -4,4 +4,8 @@ import bg.softuni.exampreparation.model.service.UserServiceModel;
 
 public interface UserService {
     UserServiceModel registerUser(UserServiceModel userServiceModel);
+
+    UserServiceModel findByUsernameAndPassword(String username, String password);
+
+    void loginUser(Long id, String username);
 }
--- Spring/02. Spring Web/1. Spring Fundamentals/9. Exam Preparation/exam-preparation/exam-preparation/src/main/java/bg/softuni/exampreparation/service/impl/UserServiceImpl.java
+++ Spring/02. Spring Web/1. Spring Fundamentals/9. Exam Preparation/exam-preparation/exam-preparation/src/main/java/bg/softuni/exampreparation/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package bg.softuni.exampreparation.service.impl;
 import bg.softuni.exampreparation.model.entity.UserEntity;
 import bg.softuni.exampreparation.model.service.UserServiceModel;
 import bg.softuni.exampreparation.repository.UserRepository;
+import bg.softuni.exampreparation.security.CurrentUser;
 import bg.softuni.exampreparation.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,11 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
-
-    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+    private final CurrentUser currentUser;
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, CurrentUser currentUser) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.currentUser = currentUser;
     }
 
     @Override
@@ -26,5 +28,20 @@ public class UserServiceImpl implements UserService {
         UserEntity user = modelMapper.map(userServiceModel, UserEntity.class);
 
         return modelMapper.map(userRepository.save(user), UserServiceModel.class);
+    }
+
+    @Override
+    public UserServiceModel findByUsernameAndPassword(String username, String password) {
+
+        return userRepository
+                .findByUsernameAndPassword(username, password)
+                .map(user -> modelMapper.map(user, UserServiceModel.class))
+                .orElse(null);
+    }
+
+    @Override
+    public void loginUser(Long id, String username) {
+        currentUser.setId(id);
+        currentUser.setUsername(username);
     }
 }
