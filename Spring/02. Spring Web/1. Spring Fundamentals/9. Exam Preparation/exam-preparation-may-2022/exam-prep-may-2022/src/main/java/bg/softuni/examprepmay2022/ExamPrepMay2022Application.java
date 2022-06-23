7758b8ee79b97d0de2545ed29e24fcba8a52d0e0
emilaramazow <emil.aramazow@gmail.com>
2022-06-23 11:39:53 +0300

    Initial commit

---
+++ Spring/02. Spring Web/1. Spring Fundamentals/9. Exam Preparation/exam-preparation-may-2022/exam-prep-may-2022/src/main/java/bg/softuni/examprepmay2022/ExamPrepMay2022Application.java
@@ -0,0 +1,13 @@
+package bg.softuni.examprepmay2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPrepMay2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamPrepMay2022Application.class, args);
+    }
+
+}
---
+++ Spring/02. Spring Web/1. Spring Fundamentals/9. Exam Preparation/exam-preparation-may-2022/exam-prep-may-2022/src/test/java/bg/softuni/examprepmay2022/ExamPrepMay2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.examprepmay2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPrepMay2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
