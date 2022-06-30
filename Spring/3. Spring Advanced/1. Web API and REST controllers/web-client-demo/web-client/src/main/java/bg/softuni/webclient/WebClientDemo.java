68e4adcbcd088ac16836fdb3ab7dbe8ddb1df487
emilaramazow <emil.aramazow@gmail.com>
2022-06-30 10:51:25 +0300

    added webclient demo

---
+++ Spring/3. Spring Advanced/1. Web API and REST controllers/web-client-demo/web-client/src/main/java/bg/softuni/webclient/WebClientDemo.java
@@ -0,0 +1,27 @@
+package bg.softuni.webclient;
+
+import bg.softuni.webclient.model.dto.UserDTO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class WebClientDemo implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        WebClient client = WebClient.create("https://reqres.in/");
+
+        UserDTO user = client.get()
+                .uri("api/users/2")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(UserDTO.class)
+                .block();
+
+        System.out.println(user);
+    }
+}
---
+++ Spring/3. Spring Advanced/1. Web API and REST controllers/web-client-demo/web-client/src/test/java/bg/softuni/webclient/WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
