9fec3a49497d062e24ac29b2dad323e4a0c7d018
emilaramazow <emil.aramazow@gmail.com>
2022-08-14 19:28:46 +0300

    Docker demo initial commit

---
+++ Spring/03. Spring Advanced/8. Containerization & Documentation/docker-demo/src/test/java/bg/softuni/dockerdemo/DockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.dockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
