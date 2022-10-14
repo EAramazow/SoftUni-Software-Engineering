534f0df4a96acb63afd092a2f19c809cfad81399
emilaramazow <emil.aramazow@gmail.com>
2022-10-14 23:17:42 +0300

    Added custom data structures

--- Java/03. Java Advanced/07. Workshop/Lab/custom-data-structures/custom-data-structures/src/SmartArray/SmartArrayMain.java
+++ Java/03. Java Advanced/07. Workshop/Lab/custom-data-structures/custom-data-structures/src/SmartArray/SmartArrayMain.java
@@ -1,4 +1,6 @@
-public class Main {
+package SmartArray;
+
+public class SmartArrayMain {
     public static void main(String[] args) {
 
         SmartArray smartArray = new SmartArray();
@@ -10,23 +12,23 @@ public class Main {
         smartArray.add(5);
 
         // 1.
-        System.out.println("The numbers in SmartArray is: ");
+        System.out.println("The numbers in SmartArray.SmartArray is: ");
 
         smartArray.forEach(System.out::println);
 
         // 2.
-        System.out.println("The selected index from SmartArray is: ");
+        System.out.println("The selected index from SmartArray.SmartArray is: ");
         System.out.println(smartArray.get(2));
 
         smartArray.remove(4);
 
-        System.out.println("The numbers in SmartArray after remove method is: ");
+        System.out.println("The numbers in SmartArray.SmartArray after remove method is: ");
 
         smartArray.forEach(System.out::println);
 
         smartArray.insert(0, 0);
 
-        System.out.println("The numbers in SmartArray after insert method is: ");
+        System.out.println("The numbers in SmartArray.SmartArray after insert method is: ");
 
         smartArray.forEach(System.out::println);
 
