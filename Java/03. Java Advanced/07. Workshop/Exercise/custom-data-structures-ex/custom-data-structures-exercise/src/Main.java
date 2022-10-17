5789d657f4266d2d5c769b78aa90576535c88150
emilaramazow <emil.aramazow@gmail.com>
2022-10-18 00:13:42 +0300

    Added methods to LinkedListTail

--- Java/03. Java Advanced/07. Workshop/Exercise/custom-data-structures-ex/custom-data-structures-exercise/src/Main.java
+++ Java/03. Java Advanced/07. Workshop/Exercise/custom-data-structures-ex/custom-data-structures-exercise/src/Main.java
@@ -1,18 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        // LinkedList:
+
         LinkedList linkedList = new LinkedList();
 
         linkedList.addFirst(1);
         linkedList.addFirst(2);
         linkedList.addFirst(3);
 
-//        linkedList.addLast(4);
-//        linkedList.addLast(5);
-//        linkedList.addLast(6);
+        linkedList.addLast(4);
+        linkedList.addLast(5);
+        linkedList.addLast(6);
 
-//        System.out.println(linkedList.get(0));
-//        System.out.println(linkedList.get(2));
+        System.out.println(linkedList.get(0));
+        System.out.println(linkedList.get(2));
 
 
         System.out.println(linkedList.removeLast());
@@ -20,6 +22,18 @@ public class Main {
         int[] arr = linkedList.toArray();
 
         linkedList.forEach(System.out::println);
+
+
+        // LinkedListTail:
+
+        LinkedListTail linkedListTail = new LinkedListTail();
+
+        linkedListTail.addFirst(1);
+        linkedListTail.addFirst(2);
+
+        linkedListTail.removeFirst();
+
+        linkedListTail.forEach(System.out::println);
 
     }
 }
