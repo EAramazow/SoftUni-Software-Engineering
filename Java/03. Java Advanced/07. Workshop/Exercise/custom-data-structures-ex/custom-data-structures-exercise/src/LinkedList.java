80ce8324b44f806ccfaf26f8a7562403e092990f
emilaramazow <emil.aramazow@gmail.com>
2022-10-17 23:12:46 +0300

    Added forEach method for LinkedList

--- Java/03. Java Advanced/07. Workshop/Exercise/custom-data-structures-ex/custom-data-structures-exercise/src/LinkedList.java
+++ Java/03. Java Advanced/07. Workshop/Exercise/custom-data-structures-ex/custom-data-structures-exercise/src/LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.function.Consumer;
+
 public class LinkedList {
 
     private Node head;
@@ -38,6 +40,17 @@ public class LinkedList {
 
     public boolean isEmpty() {
         return this.size == 0;
+    }
+
+    public void forEach(Consumer<Integer> consumer) {
+
+        Node currentNode = this.head;
+
+        while (currentNode != null) {
+            consumer.accept(currentNode.value);
+            currentNode = currentNode.next;
+        }
+
     }
 
 }
