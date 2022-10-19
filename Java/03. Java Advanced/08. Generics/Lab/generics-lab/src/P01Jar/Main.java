8e34db06180047bf83baeba41b800dedc9e9c8a1
emilaramazow <emil.aramazow@gmail.com>
2022-10-19 20:46:47 +0300

    Added Generics

---
+++ Java/03. Java Advanced/08. Generics/Lab/generics-lab/src/P01Jar/Main.java
@@ -0,0 +1,11 @@
+package P01Jar;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Jar<Integer> integerJar = new Jar<Integer>();
+
+        integerJar.add(13);
+
+    }
+}
