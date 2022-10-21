c4ba3a82a58dd6c9f72bec1eb98bc2305bf57eaf
emilaramazow <emil.aramazow@gmail.com>
2022-10-21 19:36:11 +0300

    Added P05CustomList

---
+++ Java/03. Java Advanced/08. Generics/Exercise/generics-exercise/src/P05CustomList/Main.java
@@ -0,0 +1,51 @@
+package P05CustomList;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        CustomList<String> customList = new CustomList<>();
+
+        while (!input.equals("END")) {
+            String[] commandParts = input.split(" ");
+            String commandName = commandParts[0];
+
+            switch (commandName) {
+                case "Add":
+                    String elementToAdd = commandParts[1];
+                    customList.add(elementToAdd);
+                    break;
+                case "Remove":
+                    customList.remove(Integer.parseInt(commandParts[1]));
+                    break;
+                case "Contains":
+                    System.out.println(customList.contains(commandParts[1]));
+                    break;
+                case "Swap":
+                    int firstIndex = Integer.parseInt(commandParts[1]);
+                    int secondIndex = Integer.parseInt(commandParts[2]);
+                    customList.swap(firstIndex, secondIndex);
+                    break;
+                case "Greater":
+                    System.out.println(customList.countGreaterThan(commandParts[1]));
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Print":
+                    System.out.println(customList);
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
+    }
+}
