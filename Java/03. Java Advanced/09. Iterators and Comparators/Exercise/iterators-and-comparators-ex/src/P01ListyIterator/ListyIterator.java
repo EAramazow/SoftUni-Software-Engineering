0a29aefb90f043e21cdb0c4c245efa694d809208
emilaramazow <emil.aramazow@gmail.com>
2022-10-31 18:11:55 +0200

    Added P01ListyIterator

---
+++ Java/03. Java Advanced/09. Iterators and Comparators/Exercise/iterators-and-comparators-ex/src/P01ListyIterator/ListyIterator.java
@@ -0,0 +1,37 @@
+package P01ListyIterator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListyIterator {
+
+    private List<String> elements;
+    private int currentIndex;
+
+    public ListyIterator(String... elements) {
+        this.elements = Arrays.asList(elements);
+        this.currentIndex = 0;
+    }
+
+    public boolean hasNext() {
+        if (currentIndex < elements.size() - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean move() {
+        if (hasNext()) {
+            this.currentIndex++;
+            return true;
+        }
+        return false;
+    }
+
+    public void print() {
+        if (this.elements.size() == 0) {
+            throw new IllegalStateException("Invalid Operation!");
+        }
+        System.out.println(this.elements.get(currentIndex));
+    }
+}
